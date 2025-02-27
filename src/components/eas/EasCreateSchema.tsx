8a5d2bdbcb7d826f823104bf06c655601febfa91
dzobbe <mazzeo.gio@gmail.com>
2025-02-27 16:14:36 +0100

    changed types

--- src/components/eas/EasCreateSchema.tsx
+++ src/components/eas/EasCreateSchema.tsx
@@ -36,6 +36,16 @@ const dataTypes = [
     "bytes",
     "uint8",
     "uint16",
+    "uint32",
+    "uint64",
+    "uint128",
+    "uint256",
+    "int8",
+    "int16",
+    "int32",
+    "int64",
+    "int128",
+    "int256",
 ];
 
 const EasCreateSchema: React.FC<EasCreateSchemaProps> = (props) => {
